9481800e4be4cc6350add03e1038d24a2ff4e629
MJCoderMJCoder <598157378@qq.com>
2019-08-06 18:44:35 +0800

    Accessing data with MySQL： https://spring.io/guides/gs/accessing-data-mysql/

---
+++ src/main/java/com/springboot/mysql_driver/MysqlDriverApplication.java
@@ -0,0 +1,20 @@
+package com.springboot.mysql_driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class MysqlDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlDriverApplication.class, args);
+    }
+
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+    }
+}
---
+++ src/main/java/com/springboot/mysql_driver/configuration/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.springboot.mysql_driver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    /**
+     * csrf默认是开启的；会导致403
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+}
